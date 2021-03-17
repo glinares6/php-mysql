b8bc93a2b7103d0c8a3a60b2c68dc3444333abd6
gianmarco <glinaresx@gmail.com>
2021-03-17 01:07:48 -0500

    creación de una pagina en php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- en php se escribe dentro del body su estructura -->
+    <?php
+       //comentario corto  imprimir un mensaje de pantalla y salto de linea
+        print "bienvenido al curso php <br>";
+        print "hola alumno <br>";
+        print "hassta el proximo video";
+    /*
+    comentario extenso
+    afsdfasdf adfsasdfa fa df adf adf 
+    asdf asdfasdfa df ads fasdfas fa dasf
+    asdfasdf asdf 
+    asd  asd a asda  adasd as asd adsasdf 
+    */
+    ?>
+</body>
+</html>
---
+++ segunda_pagina.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>segunda pagina</title>
+</head>
+<body>
+        <!-- variable-> espacio de menoria en el navegador
+            
+        -->
+
+        <?php
+        // declarar unavariable -Resevar un espacio en memoria
+            $nombre="Gianmarco";  
+            $edad=27;
+            // para concatenar se hace por medio de punto
+            // commillas simples escribe el texto al cual es
+            // print-> concatena mediante . devuelve el valor 1
+            print "como estas ".$nombre." tu edad es ".$edad."<br>";
+            echo "mi nombre es ",$nombre," tengo ",$edad," años<br>";
+            // echo ->lo imprime mediante comas
+            echo $nombre,$edad;
+            // print -> funcion 
+            // echo ->Expresion
+
+        ?>
+</body>
+</html>
