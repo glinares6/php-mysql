a7968ec2f35cf030eb01a5a9f6c61066f5582e32
gianmarco <glinaresx@gmail.com>
2021-03-21 00:57:10 -0500

    se modifico la variable

--- 14-funcones-matematicas.php
+++ 14-funcones-matematicas.php
@@ -11,10 +11,11 @@
     // numero aleatorio. tiene 2 parametros(inicio-fin)
         // $num1=rand(1,50);
         // el casteo de string a numeros o viceverza se hace inplicitamente
-        $num0=5;
+        $num0="5";
         $num1=pow(3,4);
         $num2=round(3.476356,2);
-        echo "el casteo implito es ".num;
+        // $num0 se tranforma en un entero cateo implicito
+        echo "el casteo implito es ".$num0;
         // casteo explicito
         echo "el numero es :". (int)$num1."<br>";
         echo "el numero es :". $num2;
